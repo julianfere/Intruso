2f049f2ee44b45dd27737e82047963f9beccbca0
julianfere_ <julianferegotti96@gmail.com>
2022-07-02 22:59:06 -0300

    misc

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,22 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { io } from "socket.io-client";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
-const socket = io("localhost:3000");
+import Home from "./components/Home";
+
+import "./index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-const emitEvent = (event: string, data: any) => {
-  console.log("Emiting Event");
-  socket.emit(event, data);
-};
-
 root.render(
   <React.StrictMode>
-    <button onClick={() => emitEvent("test", "test")}>Test</button>
-    <button onClick={() => emitEvent("test1", "test1")}>Test</button>
-    <button onClick={() => emitEvent("test2", "test2")}>Test</button>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
